d47ce1ddcdfbc1e992051a462acbd3b40e71160f
Adam Chlebek <CHLEBEAD@mail.gvsu.edu>
2018-07-23 10:22:13 -0400

    Finished Base UI

--- DragAndDrop/Form1.Designer.vb
+++ DragAndDrop/Form1.Designer.vb
@@ -22,17 +22,65 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.pbIcon = New System.Windows.Forms.PictureBox()
+        Me.lblDrag = New System.Windows.Forms.Label()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.lblFilePath = New System.Windows.Forms.Label()
+        CType(Me.pbIcon, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
+        '
+        'pbIcon
+        '
+        Me.pbIcon.Image = Global.DragAndDrop.My.Resources.Resources.Upload
+        Me.pbIcon.Location = New System.Drawing.Point(228, 110)
+        Me.pbIcon.Name = "pbIcon"
+        Me.pbIcon.Size = New System.Drawing.Size(168, 150)
+        Me.pbIcon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.pbIcon.TabIndex = 1
+        Me.pbIcon.TabStop = False
+        '
+        'lblDrag
+        '
+        Me.lblDrag.AutoSize = True
+        Me.lblDrag.Font = New System.Drawing.Font("Century Gothic", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblDrag.Location = New System.Drawing.Point(183, 263)
+        Me.lblDrag.Name = "lblDrag"
+        Me.lblDrag.Size = New System.Drawing.Size(254, 28)
+        Me.lblDrag.TabIndex = 2
+        Me.lblDrag.Text = "Drag and Drop a File"
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'lblFilePath
+        '
+        Me.lblFilePath.Font = New System.Drawing.Font("Century Gothic", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblFilePath.Location = New System.Drawing.Point(12, 374)
+        Me.lblFilePath.Name = "lblFilePath"
+        Me.lblFilePath.Size = New System.Drawing.Size(607, 84)
+        Me.lblFilePath.TabIndex = 3
+        Me.lblFilePath.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
         'Form1
         '
+        Me.AllowDrop = True
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(710, 467)
+        Me.ClientSize = New System.Drawing.Size(631, 467)
+        Me.Controls.Add(Me.lblFilePath)
+        Me.Controls.Add(Me.lblDrag)
+        Me.Controls.Add(Me.pbIcon)
         Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.Text = "New File"
+        CType(Me.pbIcon, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents pbIcon As PictureBox
+    Friend WithEvents lblDrag As Label
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents lblFilePath As Label
 End Class
--- DragAndDrop/Form1.vb
+++ DragAndDrop/Form1.vb
@@ -1,3 +1,89 @@
-﻿Public Class Form1
+﻿Imports System.Runtime
+Imports System.IO
 
+Public Class Form1
+    Dim myFile As FileInfo
+
+    Private Sub Form1_DragEnter(sender As Object, e As DragEventArgs) Handles Me.DragEnter
+        If e.Data.GetDataPresent(DataFormats.FileDrop) Then
+            e.Effect = DragDropEffects.Copy
+        End If
+    End Sub
+
+    Private Sub Form1_DragDrop(sender As Object, e As DragEventArgs) Handles Me.DragDrop
+        Dim file As String = e.Data.GetData(DataFormats.FileDrop)(0)
+
+        var.myFile = New FileInfo(file)
+
+        If String.IsNullOrWhiteSpace(var.myFile.Extension) Then Exit Sub
+
+        DraggedFile()
+    End Sub
+
+    Private Sub Form1_Paint(sender As Object, e As PaintEventArgs) Handles Me.Paint
+        Dim myPen As Pen
+        myPen = New Pen(System.Drawing.Color.Gray, 5)
+
+        Dim myGraphics As Graphics = Me.CreateGraphics
+
+        myPen.DashStyle = Drawing.Drawing2D.DashStyle.Dot
+        myGraphics.DrawRectangle(myPen, 1, 1, Me.Width - 20, Me.Height - 42)
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.CenterToScreen()
+        pbIcon.Left = (Me.ClientSize.Width - pbIcon.Width) / 2
+        pbIcon.Top = (Me.ClientSize.Height - pbIcon.Height) / 2 - 50
+    End Sub
+
+    Private Sub Form1_Click(sender As Object, e As EventArgs) Handles Me.Click
+        GetFile()
+    End Sub
+
+    Private Sub lblDrag_Click(sender As Object, e As EventArgs) Handles lblDrag.Click
+        GetFile()
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles pbIcon.Click
+        GetFile()
+    End Sub
+
+    Public Sub DraggedFile()
+        lblDrag.Text = "File Loaded"
+        lblDrag.Left = (Me.ClientSize.Width - lblDrag.Width) / 2
+        lblFilePath.Text = var.myFile.FullName
+        lblFilePath.Left = (Me.ClientSize.Width - lblFilePath.Width) / 2
+    End Sub
+
+    Private Sub pbIcon_MouseHover(ByVal sender As Object, ByVal e As System.EventArgs) Handles pbIcon.MouseHover
+        pbIcon.Width = 202
+        pbIcon.Height = 180
+        pbIcon.Left = (Me.ClientSize.Width - pbIcon.Width) / 2
+        pbIcon.Top = (Me.ClientSize.Height - pbIcon.Height) / 2 - 50
+    End Sub
+
+    Private Sub pbIcon_MouseLeave(ByVal sender As Object, ByVal e As System.EventArgs) Handles pbIcon.MouseLeave
+        pbIcon.Width = 168
+        pbIcon.Height = 150
+        pbIcon.Left = (Me.ClientSize.Width - pbIcon.Width) / 2
+        pbIcon.Top = (Me.ClientSize.Height - pbIcon.Height) / 2 - 50
+    End Sub
+
+    Public Sub GetFile()
+        Dim fileName As String
+        Dim openFileDialog1 As OpenFileDialog = New OpenFileDialog()
+
+        'openFileDialog1.Filter = "Excel or CSV (*.csv; *.xlsx)|*.csv; *.xlsx"
+        'openFileDialog1.Title = "Select a File"
+
+        If openFileDialog1.ShowDialog() = System.Windows.Forms.DialogResult.OK Then
+            fileName = openFileDialog1.FileName
+            var.myFile = New FileInfo(fileName)
+
+            lblDrag.Text = "File Loaded"
+            lblDrag.Left = (Me.ClientSize.Width - lblDrag.Width) / 2
+            lblFilePath.Text = var.myFile.FullName
+            lblFilePath.Left = (Me.ClientSize.Width - lblFilePath.Width) / 2
+        End If
+    End Sub
 End Class
